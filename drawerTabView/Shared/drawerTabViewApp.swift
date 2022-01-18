2c252691f01d61f2404f3b50f1686726d2d8b30e
Zack Jung <ckse93@gmail.com>
2022-01-18 10:39:06 -0600

    inital commit

---
+++ drawerTabView/Shared/drawerTabViewApp.swift
@@ -0,0 +1,17 @@
+//
+//  drawerTabViewApp.swift
+//  Shared
+//
+//  Created by Zack Jung on 1/18/22.
+//
+
+import SwiftUI
+
+@main
+struct drawerTabViewApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
